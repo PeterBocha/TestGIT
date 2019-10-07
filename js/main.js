f8cdf1eeba876130380651d0ef764c59d526c829
Peter <bocharow1987@gmail.com>
2019-10-08 00:42:32 +0400

    Add nav into main-menu and style for it

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
+let list = document.querySelector('#list');
+let listChild = list.children;
+
+function creatspan(elem) {
+    let span = document.createElement('elem');
+    span.classList.add('nav');
+    span.innerHTML = "v";
+    return span
+}
+
+for (let i = 0; i <= listChild.length; i++) {
+    listChild[i].appendChild(creatspan('SPAN'));
+}
+
+
